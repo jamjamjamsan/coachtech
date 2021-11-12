eed25f35c7b33540aacd1a60de16790454ed674c
yoshimoto <kurakuenjam@aol.com>
2021-11-13 05:38:05 +0900

    first-commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let test = [kokugo, suugaku, eigo];
+console.log(test.push("rika"));
+console.log(test.length);
+
+for (i = 1; i <= 50; i++){
+  if (i % 15 == 0) {
+    console.log("fizzbuzz");
+  }
+  else if (i % 5 == 0) {
+    console.log("buzz");
+
+  }
+  else (i % 3 == 0) {
+    console.log("fizz");
+  }
+}
+
+let water = 1000;
